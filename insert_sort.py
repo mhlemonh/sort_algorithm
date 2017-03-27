57cc6199e8cf559e1a017b21c064729808e55c0e
Raymond Lin <ck.lemon.l@gmail.com>
2017-03-27 07:11:26 -0700

    Add insert sort.

--- insert_sort.py
+++ insert_sort.py
@@ -5,22 +5,30 @@ def insert_sort(list_to_sort):
     index = binary_search(list_to_sort)
 
 def binary_search(sorted_list, value):
+    """ Return the index just smaller than the value."""
     end = len(sorted_list)-1
     start = 0
     search_done = False
     for i in range(7):
     #while not search_done:
         mid = (end+start)/2
-        if sorted_list[mid] < value and sorted_list[mid+1] <= value:
+        print start, mid, end
+        if sorted_list[mid] == value:
+            print 'A'
+            return mid-1
+        elif start == end:
+            return mid-1
+        elif sorted_list[mid] < value and sorted_list[mid+1] < value:
+            print 'B'
             start = mid
-        elif sorted_list[mid] > value and sorted_list[mid+1] >= value:
+        elif value < sorted_list[mid] and value < sorted_list[mid+1]:
+            print 'C'
             end = mid
         else:
-            search_done = True
-            break
+            return mid
     return mid
 if __name__ == '__main__':
     list_to_sort = [5,1,7,4,6,2,9,3,8]
     sl = [1,2,3,4,5,6,7,8,9,10]
-    print binary_search(sl,-9.8)
+    print binary_search(sl,-100)
     #insert_sort()
