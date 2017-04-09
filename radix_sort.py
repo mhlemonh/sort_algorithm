1bd3eb9b5ebf917a4d5590b60a2f524a24a8a96d
Raymond <ck.lemon.l@gmail.com>
2017-04-09 14:36:13 +0800

    Finish radix sort

--- radix_sort.py
+++ radix_sort.py
@@ -9,19 +9,18 @@ def max_redix(list_to_sort, base=10):
 def radix_sort(list_to_sort, base=10):
     iter_time = max_redix(list_to_sort, base)
     merged_bucket = list_to_sort
-    bucket = [[] for i in range(base)]
     for i in range(iter_time):
+        bucket = [[] for j in range(base)]
         for item in merged_bucket:
-            bucket_number = item // (base**i)
+            bucket_number = (item // (base**i)) % base
             bucket[bucket_number].append(item)
-
-        merged_bucket = reduce(apend)
-    return sorted_list
+        merged_bucket = sum(bucket, [])
+    return merged_bucket
 
 if __name__ == '__main__':
     range_min, range_max = 0, 300
-    l=np.random.randint(range_min, range_max, size=9).tolist()
+    np.random.seed(9487)
+    l=np.random.randint(range_min, range_max, size=10).tolist()
     print "Initial list:", l
-    print max_redix(l, 2)
-    #sl = radix_sort(l)
-    #print " Sorted list:", sl
+    sl = radix_sort(l, 2)
+    print " Sorted list:", sl
